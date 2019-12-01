d059d582ce2b7ab3c8154ed1fee762b574925b7b
Daniel Parks <dp+git@oxidized.org>
2019-11-30 20:28:00 -0800

    Benchmarks.

---
+++ benchmarks/encode.rs
@@ -0,0 +1,29 @@
+use bencher::*;
+use htmlentity::*;
+
+const SMALL_DIRTY: &str = "<a href=\"http://example.com/\">link</a> & [link]";
+const SMALL_CLEAN: &str = ".a href=.http://example.com/..link./a. . [link]";
+const BIG_DIRTY: &str = include_str!("../tests/corpus/html-raw.txt");
+const BIG_CLEAN: &str = include_str!("../tests/corpus/html-cleaned.txt");
+
+macro_rules! bench_func {
+    ($name:ident, $func:ident, $sample:expr) => {
+        fn $name(bench: &mut Bencher) {
+            let sample = $sample;
+            bench.iter(|| {
+                encode_text(sample);
+            });
+            bench.bytes = sample.len() as u64;
+        }
+    }
+}
+
+bench_func!(encode_text_small_dirty, encode_text, SMALL_DIRTY);
+bench_func!(encode_text_small_clean, encode_text, SMALL_CLEAN);
+bench_func!(encode_text_big_dirty, encode_text, BIG_DIRTY);
+bench_func!(encode_text_big_clean, encode_text, BIG_CLEAN);
+
+benchmark_group!(benches,
+    encode_text_small_dirty, encode_text_small_clean,
+    encode_text_big_dirty, encode_text_big_clean);
+benchmark_main!(benches);
