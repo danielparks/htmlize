7bb49c990ff4a2e92f52a160904013c9f23328e1
Daniel Parks <dp+git@oxidized.org>
2023-01-13 07:49:45 -0800

    Reorganize escape benchmarks for easier comparison.

--- benches/util/inputs.rs
+++ benches/util/inputs.rs
@@ -1,7 +1,7 @@
-pub const SMALL_CLEAN: &str = "Björk and Борис OBrien ❤️, “love beats hate”";
-pub const MEDIUM_CLEAN: &str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.";
-pub const BIG_CLEAN: &str = include_str!("../../tests/corpus/html-cleaned.txt");
+pub const CLEAN_SMALL: &str = "Björk and Борис OBrien ❤️, “love beats hate”";
+pub const CLEAN_MEDIUM: &str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.";
+pub const CLEAN_BIG: &str = include_str!("../../tests/corpus/html-cleaned.txt");
 
-pub const SMALL_DIRTY: &str = r#"Björk & Борис O'Brien <3, "love > hate""#;
-pub const MEDIUM_DIRTY: &str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa>";
-pub const BIG_DIRTY: &str = include_str!("../../tests/corpus/html-raw.txt");
+pub const DIRTY_SMALL: &str = r#"Björk & Борис O'Brien <3, "love > hate""#;
+pub const DIRTY_MEDIUM: &str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa>";
+pub const DIRTY_BIG: &str = include_str!("../../tests/corpus/html-raw.txt");
